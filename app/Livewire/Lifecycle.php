028abee11bae7896682d8977c2579a2b329f5923
fallah-rasool <fallah.rasol@gmail.com>
2025-05-24 01:59:31 +0330

    Boot Hooks

--- app/Livewire/Lifecycle.php
+++ app/Livewire/Lifecycle.php
@@ -2,7 +2,7 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Lifecycle extends Component
@@ -12,7 +12,10 @@ class Lifecycle extends Component
     public $mount_id ;
     public $mount_request;
 
-    public function mount(string $id = null,Request $request)
+    public int $boot_time;
+    public int $boot_calls = 0;
+
+    public function mount(string $id = null)
     {
       
         //on Component Create
@@ -21,8 +24,15 @@ class Lifecycle extends Component
 
         $this->mount_called ++;
         $this->mount_id= $id;
-        $this->mount_request= $request;
 
+    }
+
+    public function boot(): void
+    {
+        // executes on beginning of every request (initial, subsequently)
+
+        $this->boot_time = time();
+        $this->boot_calls++;
     }
 
     public function render()
--- resources/views/livewire/lifecycle.blade.php
+++ resources/views/livewire/lifecycle.blade.php
@@ -1,5 +1,12 @@
 <div class= "max-w-xl px-5 m-5 mx-auto " > 
     <strong>Lifecycle</strong>
+    <br>
+    <br>
+    <button wire:click.prevent="$refresh"
+    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2"
+    >
+        Refresh
+    </button>
   
     <hr style="color: #000;border: #000 solid 1px;margin: 10px 0;">
 
@@ -8,9 +15,18 @@
     <p>Creation Time : {{ $creation_time }}</p>
     <p>Mount Called : {{ $mount_called}}</p>
     <p>Mount id : {{ $mount_id??'not set' }}</p>
-    <p>Mount Request : {{ $mount_request}}</p>
+    {{-- <p>Mount Request : {{ $mount_request}}</p> --}}
 
   
     <hr style="color: #000;border: #929292 solid 1px;margin: 10px 0;">
     {{-- The whole world belongs to you. --}}
+
+    <h1> <strong>Boot Hooks</strong> </h1>
+
+    <p>
+        Last Boot Time: {{ $boot_time }}
+    </p>
+    <p>
+        Boot Calls: {{ $boot_calls }} times
+    </p>
 </div>
